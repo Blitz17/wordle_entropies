c6ca491d03092e1ae15128421f6f1752adff0177
Blitz17 <110586053+Blitz17@users.noreply.github.com>
2024-05-15 18:23:37 +0530

    guess-wordle done

--- wordle-it-guesswork.py
+++ wordle-it-guesswork.py
@@ -1,4 +1,6 @@
 import numpy as np 
+from tqdm import tqdm
+import time
 
 class State :
     in_progress = 0
@@ -11,25 +13,29 @@ class letter_result :
     green = 1
 
 class Wordle :
-    max_guesses = 6
+    max_guesses = 14855
     guesses = []
     answer = ""
+    possible_words = []
     
-    def create(self, answer = "cream") :
+    def create(self, possible_words, answer = "cream") :
         self.answer = answer
+        self.possible_words = possible_words
+        self.guesses = []
         return self
 
     def state(self) :
-        if len(self.guesses) > self.max_guesses :
-            return State.loss
-        elif self.answer in guesses : 
+        if self.answer in self.guesses :
             return State.win
+        elif len(self.guesses) >= self.max_guesses : 
+            return State.loss
         else :
             return State.in_progress
     
     def guess(self, guess) :
-        if guess not in guesses : 
+        if guess not in self.guesses : 
             self.guesses.append(guess)
+            self.possible_words.remove(guess)
         
         results = []
 
@@ -39,7 +45,7 @@ class Wordle :
             elif guess_letter in self.answer :
                 results.append(letter_result.yellow)
             else :
-                result.append(letter_result.grey)
+                results.append(letter_result.grey)
         
         word_information = []
         for (position, letter) in enumerate(guess) : 
@@ -47,8 +53,8 @@ class Wordle :
         
         return word_information
 
-def make_guess(candidates): 
-    guess = np.random.choice(candidates)
+def make_guess(possible_words): 
+    guess = np.random.choice(possible_words)
     return guess
 
 words = []
@@ -58,4 +64,27 @@ with open("words.txt", "r") as file :
         line_words = line.split()
         words.extend(line_words)
 
-print(len(words))
+class WordleBot :
+    possible_words = []
+    def play(self, wordle) :
+        while wordle.state() == State.in_progress :
+            self.possible_words = wordle.possible_words
+            guess_word = make_guess(self.possible_words)
+            results = wordle.guess(guess_word)
+        return len(wordle.guesses)
+
+    def simulate_play(self) :
+        results = []
+        for answer in tqdm(words[0:5], desc = "Guessing"):
+            all_words = words.copy()
+            wordle_answer = Wordle().create(all_words, answer)
+            num_guesses = self.play(wordle_answer)
+            results.append([answer, num_guesses])
+            # time.sleep(0.001)
+        
+        with open('output.txt', 'w') as file:
+            for result in results:
+                file.write(str(result) + '\n')
+
+Dhanush = WordleBot()
+Dhanush.simulate_play()
