9fe8d2b96e3462c1642f61fa23c04f79d0a4cde2
Blitz17 <110586053+Blitz17@users.noreply.github.com>
2024-12-17 21:36:44 +0530

    started pandas

---
+++ Actual/Codes/pandas_script.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+
+with open("Actual/Codes/answer_entropies.txt", "r", encoding="utf8") as file :
+    entropies = file.readline()
--- Actual/Codes/trial.py
+++ Actual/Codes/trial.py
@@ -1,16 +1,23 @@
 import ast
 
-with open("output_answer_hard.txt", "r", encoding="utf8") as file :
+# with open("output_answer_hard.txt", "r", encoding="utf8") as file :
+#     lines = file.readlines()
+
+# sum = 0
+# count = 0
+# for line in lines:
+#     line_list = ast.literal_eval(line)
+#     if line_list[2] == 1:
+#         sum += line_list[1]
+#         count += 1
+
+# print(sum)
+# print(count)
+# print(sum/count)
+
+with open("Actual/Codes/answer_entropies.txt", "r", encoding="utf8") as file :
     lines = file.readlines()
 
-sum = 0
-count = 0
-for line in lines:
-    line_list = ast.literal_eval(line)
-    if line_list[2] == 1:
-        sum += line_list[1]
-        count += 1
-
-print(sum)
-print(count)
-print(sum/count)
+x = ast.literal_eval(lines[0])
+list = sorted(x.items(), key=lambda item: item[1], reverse=True)
+print(list[0:25])
